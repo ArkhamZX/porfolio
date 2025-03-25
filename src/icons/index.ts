a642e8ad705d0ad6df8f44122de755157b272ebb
[ArkhamZX] <joseandres.0017@gmail.com>
2025-03-24 22:35:31 -0400

    Tags Initial types

---
+++ src/icons/index.ts
@@ -0,0 +1,85 @@
+// Frontend icons
+import JavaScriptIcon from './Front-icons/JavaScript.astro';
+import TypeScriptIcon from './Front-icons/TypeScript.astro';
+import ReactIcon from './Front-icons/React.astro';
+import NextJSIcon from './Front-icons/NextJS.astro';
+import HTMLIcon from './Front-icons/HTML.astro';
+import CSSIcon from './Front-icons/CSS.astro';
+import BootstrapIcon from './Front-icons/Bootstrap.astro';
+import TailwindIcon from './Front-icons/Tailwind.astro';
+
+// Backend icons
+import CsharpIcon from './Backend-icons/Csharp.astro';
+import ExpressIcon from './Backend-icons/Express.astro';
+import NodeJSIcon from './Backend-icons/NodeJS.astro';
+import NetIcon from './Backend-icons/Net.astro';
+import NPMIcon from './Backend-icons/NPM.astro';
+import PNPMIcon from './Backend-icons/PNPM.astro';
+
+// Database icons
+import MySQLIcon from './Database-icons/MySQL.astro';
+import MongoDBIcon from './Database-icons/MongoDB.astro';
+import PostgreSQLIcon from './Database-icons/PostgreSQL.astro';
+import SQLServerIcon from './Database-icons/SQLServer.astro';
+import SQLiteIcon from './Database-icons/SQLite.astro';
+
+// Tools icons
+import GitHubIcon from '../icons/GitHub.astro';
+import PostmanIcon from './Tools-icons/Postman.astro';
+import HerokuIcon from './Tools-icons/Heroku.astro';
+import VercelIcon from './Tools-icons/Vercel.astro';
+import AWSIcon from './Tools-icons/AWS.astro';
+import DockerIcon from './Tools-icons/Docker.astro';
+import FigmaIcon from './Tools-icons/Figma.astro';
+import FirebaseIcon from './Tools-icons/Firebase.astro';
+import SupabaseIcon from './Tools-icons/Supabase.astro';
+import AzureIcon from './Tools-icons/Azure.astro';
+
+// Learning icons
+import VueJSIcon from './Learning-icons/VueJS.astro';
+import AngularIcon from './Learning-icons/Angular.astro';
+import AstroJSIcon from './Learning-icons/AstroJS.astro';
+import PythonIcon from './Learning-icons/Python.astro';
+import DjangoIcon from './Learning-icons/Django.astro';
+import NestJSIcon from './Learning-icons/NestJS.astro';
+import GraphQLIcon from './Learning-icons/GraphQL.astro';
+
+// Export all icons
+export {
+  JavaScriptIcon,
+  TypeScriptIcon,
+  ReactIcon,
+  NextJSIcon,
+  HTMLIcon,
+  CSSIcon,
+  BootstrapIcon,
+  TailwindIcon,
+  CsharpIcon,
+  ExpressIcon,
+  NodeJSIcon,
+  NetIcon,
+  NPMIcon,
+  PNPMIcon,
+  MySQLIcon,
+  MongoDBIcon,
+  PostgreSQLIcon,
+  SQLServerIcon,
+  SQLiteIcon,
+  GitHubIcon,
+  PostmanIcon,
+  HerokuIcon,
+  VercelIcon,
+  AWSIcon,
+  DockerIcon,
+  FigmaIcon,
+  FirebaseIcon,
+  SupabaseIcon,
+  AzureIcon,
+  VueJSIcon,
+  AngularIcon,
+  AstroJSIcon,
+  PythonIcon,
+  DjangoIcon,
+  NestJSIcon,
+  GraphQLIcon,
+};
